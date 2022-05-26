31c7c88a42e3ce93ba196a98013f76bad8a3acb8
Arnab <arnab.kundu1990@gmail.com>
2022-05-26 09:54:50 +0530

    PanoramaImageView is a 360 degree image view in android - This project was working on Android Studio v3.3.0 Updates - gradle dependency versions - migrated to AndroidX - updated compileSdkVersion to 31, - updated minSdkVersion to 26 - Now compiled, build successful, running in Android Studio v2020.3.1

--- app/src/main/java/com/gjiazhe/panoramaimageview/sample/HorizontalSampleActivity.java
+++ app/src/main/java/com/gjiazhe/panoramaimageview/sample/HorizontalSampleActivity.java
@@ -1,6 +1,6 @@
 package com.gjiazhe.panoramaimageview.sample;
 
-import android.support.v7.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
 import com.gjiazhe.panoramaimageview.GyroscopeObserver;
